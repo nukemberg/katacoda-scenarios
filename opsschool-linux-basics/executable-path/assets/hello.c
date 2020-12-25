2371dc2aa28928451e693288043636f41a8fc0e0
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2020-12-25 15:47:06 +0200

    add progname

--- opsschool-linux-basics/executable-path/assets/hello.c
+++ opsschool-linux-basics/executable-path/assets/hello.c
@@ -2,6 +2,11 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
+#define _GNU_SOURCE
+#include <errno.h>
+
+extern char *program_invocation_name;
+extern char *program_invocation_short_name;
 
 int main(int argc, char **argv) {
     printf("Hello world!\n");
@@ -11,6 +16,8 @@ int main(int argc, char **argv) {
     }
 
     printf("PID: %d\n", getpid());
+    printf("PPID: %d\n", getppid());
+    printf("Program name: %s\n", program_invocation_name);
 
     int i = 0;
     char *env = __environ[i];
